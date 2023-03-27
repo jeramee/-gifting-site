2600cfaa0cae00800642d8779c067efd76e73efe
CR <41214268+DJBrohawk@users.noreply.github.com>
2023-03-26 22:57:39 -0400

    starting framework for rotating item array

--- js/main.js
+++ js/main.js
@@ -1,19 +1,51 @@
-let boo = false;
-sessionStorage.setItem("boo", "false");
+
+
 
 
 function login() {
 console.log("SURPRISE BOO");
-    if (boo === false){
-        boo = true;
-    } else {
-        boo = false;
-    }
 
-    if (boo) {
 
+if (sessionStorage.getItem("boo") === 'false'){
+
+sessionStorage.setItem("boo", "true");
+
+} else if (sessionStorage.getItem("boo") === 'true') {
+
+sessionStorage.setItem("boo", "false");
+}
+
+
+    console.log(boo);
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+
+
+    if (sessionStorage.getItem("boo") === 'false') {
+
+        console.log("helpsda");
         document.getElementById("mainnav").innerHTML = `
         
+       
+
+           
+        <a href="browse.html">Browse For Items</a>
+        <a href="friendLists.html">Manage Friend List</a>
+        <a href="contactUs.html">Contact Us</a>
+        <a href="about.html">About</a>
+        <a href="howToUse.html">How to Use This Site</a>
+        <a href="logIn.html" id="login">Log In</a>`;
+
+     
+    
+    } else if (sessionStorage.getItem("boo") === 'true') {
+
+        console.log("help");
+   
+        document.getElementById("mainnav").innerHTML = `
+
         <a href="browse.html">Browse For Items</a>
         <a href="friendLists.html">Manage Friend List</a>
         <a href="contactUs.html">Contact Us</a>
@@ -21,11 +53,12 @@ console.log("SURPRISE BOO");
         <a href="howToUse.html">How to Use This Site</a>
         <a href="accountInfo.html" class="acct"><img src="acct.png" height="40px" width="40px"></a>`;
      
-    
+
     }
 
-    console.log(boo);
 
-}
+
+
+});
 
 document.getElementById("login").addEventListener("click",login);
